bbd94db5ce78a32ea4087e6d34e65610833c41d3
planeta-nisshin <planeta19950228@gmail.com>
2023-03-30 12:09:11 +0900

    basic認証

--- middleware.ts
+++ middleware.ts
@@ -5,18 +5,18 @@ export const config = {
 }
 
 export function middleware(req: NextRequest) {
-    // const basicAuth = req.headers.get('authorization')
-    // const url = req.nextUrl
+    const basicAuth = req.headers.get('authorization')
+    const url = req.nextUrl
 
-    // if (basicAuth) {
-    //     const authValue = basicAuth.split(' ')[1]
-    //     const [user, pwd] = atob(authValue).split(':')
+    if (basicAuth) {
+        const authValue = basicAuth.split(' ')[1]
+        const [user, pwd] = atob(authValue).split(':')
 
-    //     if (user === process.env.NEXT_PUBLIC_ADMIN && pwd === process.env.NEXT_PUBLIC_PW) {
-    //         return NextResponse.next()
-    //     }
-    // }
-    // url.pathname = '/api/auth'
+        if (user === process.env.NEXT_PUBLIC_ADMIN && pwd === process.env.NEXT_PUBLIC_PW) {
+            return NextResponse.next()
+        }
+    }
+    url.pathname = '/api/auth'
 
-    // return NextResponse.rewrite(url)
+    return NextResponse.rewrite(url)
 }
