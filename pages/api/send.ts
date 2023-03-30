c3d9c5b7bed1d2750f078f23641746cbf2ef47ab
planeta-nisshin <planeta19950228@gmail.com>
2023-03-30 11:20:51 +0900

    送信機能修正

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,35 +1,40 @@
-export default function handler(req: any, res: any) {
+import sgMail from '@sendgrid/mail'
+import { MailDataRequired } from '@sendgrid/helpers/classes/mail'
+import { EmailData } from '@sendgrid/helpers/classes/email-address'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
     let response = null;
 
     if (req.method === 'POST') {
-        const sgMail = require('@sendgrid/mail');
-        sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_APIKEY);
-        const msg = {
-            to: req.body.email,
-            bcc: process.env.NEXT_PUBLIC_MAIL_SEND_ADDRESS,
-            from: {
-                name: 'シナップス',
-                email: process.env.NEXT_PUBLIC_MAIL_ADDRESS,
+        sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_APIKEY as string)
+        const messages = [
+            {
+                to: req.body.email,
+                bcc: process.env.NEXT_PUBLIC_MAIL_SEND_ADDRESS,
+                from: {
+                    name: 'シナップス',
+                    email: process.env.NEXT_PUBLIC_MAIL_ADDRESS,
+                } as EmailData,
+                subject: '【平針スイミングスクール】お問合せありがとうございました。',
+                text: `${req.body.name} 様\n\nこの度は、お問い合わせをいただき誠にありがとうございます。\n以下内容を受け付けました。\n-----------------------------\n\n【お名前】${req.body.name}\n\n【フリガナ】${req.body.kana}\n\n【メールアドレス】${req.body.email}\n\n【郵便番号】${req.body.zipcode.sub ? req.body.zipcode.sub : ""}\n\n【住所】${req.body.address}\n\n【電話番号】${req.body.tell}\n\n【お問い合わせ内容】\n${req.body.message}\n\n-----------------------------\n\n-- \nこのメールは (https://www.synapps.jp/) のお問い合わせフォームから送信されました`
             },
-            subject: '【平針スイミングスクール】お問合せありがとうございました。',
-            text: `${req.body.name} 様\n\nこの度は、お問い合わせをいただき誠にありがとうございます。\n以下内容を受け付けました。\n-----------------------------\n\n【お名前】${req.body.name}\n\n【フリガナ】${req.body.kana}\n\n【メールアドレス】${req.body.email}\n\n【郵便番号】${req.body.zipcode.sub ? req.body.zipcode.sub : ""}\n\n【住所】${req.body.address}\n\n【電話番号】${req.body.tell}\n\n【お問い合わせ内容】\n${req.body.message}\n\n-----------------------------\n\n-- \nこのメールは (https://www.synapps.jp/) のお問い合わせフォームから送信されました`
-        };
-        const msg2 = {
-            to: process.env.NEXT_PUBLIC_MAIL_SEND_ADDRESS,
-            bcc: '',
-            from: process.env.NEXT_PUBLIC_MAIL_ADDRESS,
-            subject: 'シナップスHPからお問合せがありました',
-            text: `ホームページからお問い合わせが届きました。\n\n内容は以下になります。\n-----------------------------\n\n【お名前】${req.body.name}\n\n【フリガナ】${req.body.kana}\n\n【メールアドレス】${req.body.email}\n\n【郵便番号】${req.body.zipcode.sub ? req.body.zipcode.sub : ""}\n\n【住所】${req.body.address}\n\n【電話番号】${req.body.tell}\n\n【お問い合わせ内容】\n${req.body.message}\n\n-----------------------------\n\n-- \nこのメールは (https://www.synapps.jp/) のお問い合わせフォームから送信されました`
-        };
+            {
+                to: process.env.NEXT_PUBLIC_MAIL_SEND_ADDRESS,
+                bcc: '',
+                from: process.env.NEXT_PUBLIC_MAIL_ADDRESS as EmailData,
+                subject: 'シナップスHPからお問合せがありました',
+                text: `ホームページからお問い合わせが届きました。\n\n内容は以下になります。\n-----------------------------\n\n【お名前】${req.body.name}\n\n【フリガナ】${req.body.kana}\n\n【メールアドレス】${req.body.email}\n\n【郵便番号】${req.body.zipcode.sub ? req.body.zipcode.sub : ""}\n\n【住所】${req.body.address}\n\n【電話番号】${req.body.tell}\n\n【お問い合わせ内容】\n${req.body.message}\n\n-----------------------------\n\n-- \nこのメールは (https://www.synapps.jp/) のお問い合わせフォームから送信されました`
+            },
+        ]
+        console.log('req.body: ', req.body);
         (async () => {
             try {
-                response = await sgMail.send(msg);
-                response = await sgMail.send(msg2);
-            } catch (error: any) {
-                console.error(error);
-                if (error.response) {
-                    console.error(error.response.body)
-                }
+                await sgMail.send(messages);
+                res.status(200).json(messages);
+            } catch (err) {
+                console.error(err);
+                res.status(500).json(err);
             }
         })();
     }
