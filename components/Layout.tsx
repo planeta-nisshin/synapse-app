8a3789ab6efcedbc2df71e47a240cdd2fffac0d4
planeta-nisshin <planeta19950228@gmail.com>
2023-03-31 15:06:31 +0900

    サーチコンソール追加

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,6 +19,7 @@ const Layout = ({ children }: {
                 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
                 <meta name="msapplication-TileColor" content="#da532c" />
                 <meta name="theme-color" content="#ffffff" />
+                <meta name="google-site-verification" content="_Ij57FIqFn1voR8lTbwiCBuamxVPf3ZU57D1On5wxKw" />
             </Head>
             <Header />
             {children}
